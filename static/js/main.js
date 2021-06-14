89c53a8bfe729169e70ab059cd233d2168334d85
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-06-14 07:41:58 +0300

    added calculateDayIndex function

--- static/js/main.js
+++ static/js/main.js
@@ -1,24 +1,23 @@
+//preventing default
+document.getElementById("submit").addEventListener("click", function(event){
+    event.preventDefault()
+    });
+
 // getting akan name
-function getAkanName(){
+function calculateDayIndex(day, month, year){
     let dd = document.getElementById("inputDate").value;
     let mm = document.getElementById("inputMonth").value;
-    let yy = document.getElementById("inputYear").value;
-    let genders = document.getElementsByName("gender");
+    let YY = document.getElementById("inputYear").value;
+    let temp = YY.split("")
+    let temp2 = temp.slice()
+    let yy = parseInt("".join(((temp.split("")).slice(0,2))))
+    let cc = parseInt("".join(((temp2.split("")).slice(2,2))))
+    let dayIndex =  ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7
 }
 //data validation
 
 
 //function to get gender
-function getGender(){
-    for(let gender of gender){
-        if(gender.checked){
-            return gender.value;
-        }
-        
-    }
-}
-let myGenderValue = getGender();
-console.log(myGenderValue);
 
 // day and name function
 function getDayAndName(){
@@ -31,4 +30,6 @@ function getDayAndName(){
     let femaleNames = [
         "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
         ];
+    let index = calculateDayIndex(dd, mm, YY)
+    let gender = document.getElementsByName("gender").value;
 }
